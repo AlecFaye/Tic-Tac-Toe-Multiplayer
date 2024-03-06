37c76a7d2d9b708675031fca6340516d6a5b090d
AlecFaye <kelvintran.lim@outlook.com>
2024-03-05 20:56:59 -0500

    Added stream chat modules and set up environment variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import { StreamChat } from "stream-chat";
+import Cookies from "universal-cookie";
 
 function App() {
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+
+    console.log("API Key: " + process.env.REACT_APP_API_KEY);
+
+    const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
+
+    if (token) {
+        client.connectUser({ 
+            id: cookies.get("userId"),
+            name: cookies.get("username"),
+            firstName: cookies.get("firstName"),
+            lastName: cookies.get("lastName"),
+            hashedPassword: cookies.get("hashedPassword")
+        }, token).then((user) => {
+            console.log(user);
+        });
+    }
+
     return (
-        <div className="App">
+        <div>
             <SignUp />
             <Login />
         </div>
     );
-}
+};
 
 export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,39 @@
 import express from "express";
 import cors from "cors";
+import { StreamChat } from "stream-chat";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config({path: "../.env"});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const api_key = 
+const api_key = process.env.REACT_APP_API_KEY;
+const api_secret = process.env.REACT_APP_API_SECRET;
+
+console.log("API Key: " + api_key);
+console.log("API Secret: " + api_secret);
+
+const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+app.post("/signup", async (req, res) => {
+    try {
+        const { firstName, lastName, username, password } = req.body;
+        const userId = uuidv4();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const token = serverClient.createToken(userId);
+    
+        res.json({ token, userId, firstName, lastName, username, hashedPassword });
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+app.post("/login");
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
